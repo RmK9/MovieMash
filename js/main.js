72a7785287164030d5468f8b31de7137b8dfd7d3
RmK9 <th3rmk@gmail.com>
2015-03-06 14:58:46 +0000

    Updated Screen scaling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
+
+	var height = $(window).height() * 0.15;
+	$(".searchBox").css({"padding-top": height});
+	
+	//Search bar onClick Event function
 	$(".input-group-btn").click(function(e){
 		e.preventDefault();
+		
 		$(".searchBox").animate({"padding-top": "40px"},"1000","swing");
 		$("#mainLogo").css({"width": "30%", "height": "30%"});
-		$("#mainLogo").animate({"padding-left": "470px"},"600");
-		//$("#mainLogo").detach().appendTo("#appender");
+		//$("#mainLogo").delay(1000).animate({"padding-left": "470px"},"600");
+		//$("#mainLogo").detach().appendTo("#appender").delay(2000);
+		
 		});
 });
